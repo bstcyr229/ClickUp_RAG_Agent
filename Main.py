3096f366120ae2e9a17674b5906f4fbc7540c20a
Bryan St. Cyr <bstcyr229@gmail.com>
2026-05-13 18:38:03 -0600

    Feat: Finished lambda chunking method

--- Main.py
+++ Main.py
@@ -14,4 +14,4 @@ import dashboard as dashboard
 
 final_data_frame_from_dashboard = dashboard.aggregrate_task_data(dashboard.fetching_tasks(dashboard.user_input()))
 final_data_frame_from_dashboard = final_data_frame_from_dashboard[0]
-final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}", axis=1 )
+final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, x{['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, x{['task_due_date']} ",  axis=1 )
