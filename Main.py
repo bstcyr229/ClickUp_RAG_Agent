4df25abf8beb90a4844e0e28d413f171e7543820
Bryan St. Cyr <bstcyr229@gmail.com>
2026-05-20 07:02:13 -0600

    Debug: converted ids to be added into chromadb into a list of strings using typecasting and .tolist() method, removed brackets from final_df collection add to resolve type error

--- Main.py
+++ Main.py
@@ -17,13 +17,15 @@ final_df_collection = client.get_or_create_collection(name="clickup_final_df_col
 final_data_frame_from_dashboard = dashboard.aggregrate_task_data(dashboard.fetching_tasks(dashboard.user_input()))
 final_data_frame_from_dashboard = final_data_frame_from_dashboard[0]
 sentence_to_be_chunked = final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, {x['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, {x['task_due_date']} ",  axis=1 ).to_string() 
-final_data_frame_from_dashboard.index
 
-print(type(sentence_to_be_chunked))
+
+ids_as_strings = final_data_frame_from_dashboard.index.astype(str).tolist()
+# ids_as_strings = ", ".join(ids_as_strings)
+# print(f"AFTER {type(ids_as_strings)} + AND THIS IS WHAT IT LOOKS LIKE {ids_as_strings}")
 
 # final_df_collection.add(
-#     ids=[],
-#     documents=[sentence_to_be_chunked],
-#     metadatas=[final_data_frame_from_dashboard.apply(lambda x: x.to_dict())] 
+#     ids= ids_as_strings,
+#     documents= sentence_to_be_chunked,
+#     metadatas= final_data_frame_from_dashboard.apply(lambda x: x.to_dict()) 
 
 # )
