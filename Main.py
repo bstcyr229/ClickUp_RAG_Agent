570df8a5f350701358ae4dac5ec5515a23758b9f
Bryan St. Cyr <bstcyr229@gmail.com>
2026-05-25 14:19:51 -0600

    Debug: metadatas collection.add attribute error fixed by using the orient records param in the to_dict method on data from the final data frame

--- Main.py
+++ Main.py
@@ -17,8 +17,7 @@ final_df_collection = client.get_or_create_collection(name="clickup_final_df_col
 final_data_frame_from_dashboard = dashboard.aggregrate_task_data(dashboard.fetching_tasks(dashboard.user_input()))
 final_data_frame_from_dashboard = final_data_frame_from_dashboard[0]
 sentence_to_be_chunked = final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, {x['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, {x['task_due_date']} . ",  axis=1 ).to_list()
-metadatas = final_data_frame_from_dashboard.columns.to_list()
-metadatas = ",".join(metadatas)
+metadatas = final_data_frame_from_dashboard.to_dict(orient='records')
 ids_as_strings = final_data_frame_from_dashboard.index.astype(str).tolist()
 
 final_df_collection.add(
