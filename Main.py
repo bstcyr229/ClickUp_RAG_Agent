0e26ce5efac5113aa9f1eb0dadbc61cbb59d6005
Bryan St. Cyr <bstcyr229@gmail.com>
2026-05-27 19:19:26 -0600

    Feature: Adding in embedding function for final df collection

--- Main.py
+++ Main.py
@@ -3,19 +3,20 @@ import chromadb
 import streamlit as st
 import numpy
 import pandas as pd 
-# from sentence_transformers import SentenceTransformer
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 
 client = chromadb.PersistentClient(path="./chroma_db")
-final_df_collection = client.get_or_create_collection(name="clickup_final_df_collection")
+
+
+final_df_collection = client.get_or_create_collection(name="final_df_collection")
+final_df_collection(
+    embedding_function=OpenAIEmbeddingFunction(
+        model_name="text-embedding-3-small"
+    )
+)
 user_input_collection = client.get_or_create_collection(name='user_input_collection')
 
-user_input = {
-    "User Input": "",
-}
-user_input_collection.query(
-    user_input_to_be_vectorized = user_input,
-    n_results=1
-)
+user_input = input("Please enter your question: ")
 
 
 
@@ -25,6 +26,8 @@ final_data_frame_from_dashboard = final_data_frame_from_dashboard[0]
 sentence_to_be_chunked = final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, {x['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, {x['task_due_date']} . ",  axis=1 ).to_list()
 metadatas = final_data_frame_from_dashboard.to_dict(orient='records')
 ids_as_strings = final_data_frame_from_dashboard.index.astype(str).tolist()
+
+final_df_collection 
 
 final_df_collection.add(
     ids= ids_as_strings,
