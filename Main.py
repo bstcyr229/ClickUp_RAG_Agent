1eadc6ce5bbf5189f2221376372e62edcac388cb
Bryan St. Cyr <bstcyr229@gmail.com>
2026-06-13 20:14:41 -0600

    Debug: Fixing duplicated varialbe in sentence to be chunked variable

--- Main.py
+++ Main.py
@@ -30,7 +30,7 @@ user_input = input("Please enter your question: ")
 
 final_data_frame_from_dashboard = dashboard.aggregrate_task_data(dashboard.fetching_tasks(dashboard.user_input()))
 final_data_frame_from_dashboard = final_data_frame_from_dashboard[0]
-sentence_to_be_chunked = final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, {x['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, {x['task_due_date']} . ",  axis=1 ).to_list()
+sentence_to_be_chunked = final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, {x['task_due_date']} . ",  axis=1 ).to_list()
 metadatas = final_data_frame_from_dashboard.to_dict(orient='records')
 ids_as_strings = final_data_frame_from_dashboard.index.astype(str).tolist()
 
@@ -49,7 +49,7 @@ def display_results(user_input):
         query_texts=user_input,
         n_results=1,)
     client = genai.Client(api_key=GEMINI_API_KEY)
-    prompt = "Never guessing, scan the documents and metadatas to answer the user query if you cannot find the information state I don't know"
+    prompt = "Never guessing, respond to the user's query by scanning the documents and metadatas to answer the user query if you cannot find the information state I don't know"
     response = client.models.generate_content(
         model="gemini-3.5-flash",
         contents=prompt
