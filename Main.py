f89925387297b5cd68e7575633ae88be8def945c
Bryan St. Cyr <bstcyr229@gmail.com>
2026-06-17 07:45:47 -0600

    Refactor: Moved the rest of the program (except for global variables and libarires) into functions, fixed get client tuple unpacking issue in load_data function

--- Main.py
+++ Main.py
@@ -15,43 +15,45 @@ load_dotenv()
 GOOGLE_GENAI_USE_VERTEXAI="false"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+
 @st.cache_resource
 def get_client():
     client = chromadb.PersistentClient(path="./chroma_db")
     gemini_ef = GoogleGeminiEmbeddingFunction(api_key_env_var = "GEMINI_API_KEY")
-    get_client_variables = gemini_ef, client
+    get_client_variables = client, gemini_ef
+    
+    return get_client_variables
 
 @st.cache_data 
-def load_data(client,gemini_ef):
+def load_data(get_client_variables):
+    client = get_client_variables[0]    
+    gemini_ef = get_client_variables[1]
     final_df_collection = client.get_or_create_collection(name="final_df_collection", embedding_function=gemini_ef
     )
     user_input_collection = client.get_or_create_collection(name='user_input_collection', embedding_function=gemini_ef)
+    final_data_frame_from_dashboard = dashboard.aggregrate_task_data(dashboard.fetching_tasks(dashboard.user_input()))
+    final_data_frame_from_dashboard = final_data_frame_from_dashboard[0]
+    sentence_to_be_chunked = final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, {x['task_due_date']} . ",  axis=1 ).to_list()
+    metadatas = final_data_frame_from_dashboard.to_dict(orient='records')
+    ids_as_strings = final_data_frame_from_dashboard.index.astype(str).tolist()
+
+
+    final_df_collection.add(
+        ids= ids_as_strings,
+        documents= sentence_to_be_chunked,
+        metadatas=metadatas
+
+    )
+    return final_df_collection
 
 
 
 
-
-user_input = input("Please enter your question: ")
-
-
-
-
-final_data_frame_from_dashboard = dashboard.aggregrate_task_data(dashboard.fetching_tasks(dashboard.user_input()))
-final_data_frame_from_dashboard = final_data_frame_from_dashboard[0]
-sentence_to_be_chunked = final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, {x['task_due_date']} . ",  axis=1 ).to_list()
-metadatas = final_data_frame_from_dashboard.to_dict(orient='records')
-ids_as_strings = final_data_frame_from_dashboard.index.astype(str).tolist()
-
-
-final_df_collection.add(
-    ids= ids_as_strings,
-    documents= sentence_to_be_chunked,
-    metadatas=metadatas
-
-)
-
-
-def display_results(user_input):
+def get_input():
+    st.write("Please enter your question: ")
+    user_input = st.text_input(label="User Input")
+    return user_input
+def display_results(user_input, final_df_collection):
 
     results = final_df_collection.query(
         query_texts=user_input,
@@ -66,5 +68,4 @@ def display_results(user_input):
     )
     
     return response
-    
-print(display_results(user_input))
+
