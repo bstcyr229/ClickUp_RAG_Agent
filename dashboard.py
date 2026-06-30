ccc5c6886fef0e7cd5d702a8cf495f25af2f57f0
Bryan <49841240+bstcyr229@users.noreply.github.com>
2026-06-29 20:11:27 -0600

    Feat: Adding in the RAG agent logic into the dashboard file to avoid key duplication error, still working on integrating the two different programs into one seamless program

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,4 @@
-
+import chromadb
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -7,12 +7,17 @@ import requests
 import os
 import json 
 import holidays
-import rag_agent
+from google import genai
+
 
 from datetime import datetime as dt, timedelta , timezone 
 
 from dotenv import load_dotenv
 load_dotenv()
+GOOGLE_GENAI_USE_VERTEXAI="false"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+from chromadb.utils.embedding_functions import GoogleGeminiEmbeddingFunction
+
 
 
 
@@ -177,6 +182,9 @@ def aggregrate_task_data(tasks_and_entries_tuple):
         dates_and_final_df = (final_df , total_work_days)
         return dates_and_final_df 
 
+
+
+
 def display_views(dates_and_final_df):
         final_df = dates_and_final_df[0]
         total_work_days = dates_and_final_df[1]
@@ -187,7 +195,6 @@ def display_views(dates_and_final_df):
 
         def view_one():
             #Team View 
-            rag_agent.display_results(rag_agent.get_input(), rag_agent.load_data(rag_agent.get_client()))
             work_day_duration = 8
             rounder = 100 
             team_members =  final_df.groupby("team_name")["team_member"].unique()
@@ -226,7 +233,6 @@ def display_views(dates_and_final_df):
 
         def view_two():
             #Staff member drill down 
-            rag_agent.display_results(rag_agent.get_input(), rag_agent.load_data(rag_agent.get_client()))
             team_member_estimated_hours_worked = final_df.groupby("team_member")["time_estimate"].sum()
             team_member_actual_hours_worked = final_df.groupby("team_member")["actual_hours"].sum()
             team_member_total_hours_worked = final_df.groupby("team_member")["actual_hours"].sum()
@@ -265,7 +271,6 @@ def display_views(dates_and_final_df):
             ).transform_fold(["Estimated", "Actual", "Billable"], as_=["variable", "value"])
             st.altair_chart(chart, width='stretch')
         def view_three():
-            rag_agent.display_results(rag_agent.get_input(), rag_agent.load_data(rag_agent.get_client()))
             tasks_for_view_three = final_df["task_name"]
             task_ids_for_view_three = final_df["task_id"]
             task_estimated_hours = final_df["time_estimate"]
@@ -316,10 +321,64 @@ def display_views(dates_and_final_df):
             view_three()
         else: 
             st.write("You selected:", genre)
-            
-            
 
+
+@st.cache_resource
+def get_client():
+    client = chromadb.PersistentClient(path="./chroma_db")
+    gemini_ef = GoogleGeminiEmbeddingFunction(api_key_env_var = "GEMINI_API_KEY")
+    _get_client_variables = client, gemini_ef
+    
+    return _get_client_variables
+
+@st.cache_resource 
+def load_data(_get_client_variables,final_df):
+    client = _get_client_variables[0]    
+    gemini_ef = _get_client_variables[1]
+    final_df_collection = client.get_or_create_collection(name="final_df_collection", embedding_function=gemini_ef
+    )
+    user_input_collection = client.get_or_create_collection(name='user_input_collection', embedding_function=gemini_ef)
+    final_data_frame_from_dashboard = final_df
+    sentence_to_be_chunked = final_data_frame_from_dashboard.apply(lambda x: f"{x['team_member']} , {x['team_member_id']} , {x['task_name']}, {x['task_id']}, {x['entry_date']}, {x['billable_hours']}, {x['non_billable']}, {x['actual_hours']}, {x['team_name']}, {x['time_estimate']}, {x['task_start_date']}, {x['task_due_date']} . ",  axis=1 ).to_list()
+    metadatas = final_data_frame_from_dashboard.to_dict(orient='records')
+    ids_as_strings = final_data_frame_from_dashboard.index.astype(str).tolist()
+
+
+    final_df_collection.add(
+        ids= ids_as_strings,
+        documents= sentence_to_be_chunked,
+        metadatas=metadatas
+
+    )
+    return final_df_collection
+def get_input():
+    st.write("Please enter your question: ")
+    user_input = st.text_input(label="User Input", key="user_input")
+    return user_input
+def display_results(user_input, final_df_collection):
+    if not user_input:
+        st.write("Please submit a question") 
+    
+    else:
+        results = final_df_collection.query(
+            query_texts=user_input,
+            n_results=10,)
+        client = genai.Client(api_key=GEMINI_API_KEY)
+        prompt = "Never guessing, respond to the user's query by scanning the documents and metadatas to answer the user query if you cannot find the information state I don't know"
+        flat_results = str(results)
+        results_prompt_user_input = [prompt, flat_results, user_input]  
+        response = client.models.generate_content(
+            model="gemini-3.1-flash-lite",
+            contents=results_prompt_user_input
+        )
+        
+        return response
+def rag_call():
+    display_results(get_input(), load_data(display_views(aggregrate_task_data(fetching_tasks(user_input_for_dashboard()))),get_client()))
 def main():
-    display_views(aggregrate_task_data(fetching_tasks(user_input_for_dashboard())))
+    display_views(aggregrate_task_data(fetching_tasks(user_input_for_dashboard()))), rag_call()
+    
+
+    
 if __name__ == "__main__":
     main()
