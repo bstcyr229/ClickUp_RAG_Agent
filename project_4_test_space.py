30294aa8020a2f4da41b40088d081da647085768
Bryan <49841240+bstcyr229@users.noreply.github.com>
2026-08-02 09:13:00 -0600

    Fix: API call func return paths, wip: adding endpoint-loop wrapper

--- project_4_test_space.py
+++ project_4_test_space.py
@@ -1,17 +1,21 @@
-import chromadb
+import os
+import datetime
+import requests
+import json 
+
 import numpy as np
 import pandas as pd
 import streamlit as st
 import altair as alt   
-import requests
-import os
-import json 
-import datetime
+
+import chromadb
+
 
 from google import genai
 from datetime import datetime as dt, timedelta , timezone 
 from dotenv import load_dotenv
 load_dotenv()
+
 # GOOGLE_GENAI_USE_VERTEXAI="false"
 # GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
@@ -41,10 +45,14 @@ class ClickUpClient:
         get_tasks_end_point= f"team/{workspace_id}/task?space_ids[]={test_space_id}" 
                                 
         def api_call_func(self, teams_end_point):
+                
+
+
+
                 get_teams_api_call = requests.get(self.base_url + teams_end_point, headers=self.headers)
                 if get_teams_api_call.status_code != 200:
-                        teams_api_end_point_error_message = print(f"User group request API call failed. ERROR CODE: {get_teams_api_call}")
-                        return print(ClickUpClient.api_call_func)   
+                        teams_api_end_point_error_message = f"User group request API call failed. ERROR CODE: {get_teams_api_call}"
+                        return print(teams_api_end_point_error_message)   
                 else:
                         user_teams_json = get_teams_api_call.json().get("groups")  
                         return(print(user_teams_json[0]['id']))
@@ -55,12 +63,11 @@ class ClickUpClient:
 def main():
         workspace_id = os.getenv("workspace_id") 
         test_space_id = os.getenv("test_space") 
-        teams_end_point = f"group?team_id={workspace_id}" #Reomve / failure injection
+        teams_end_point = f"/group?team_id={workspace_id}" #Reomve / failure injection
         #get_tasks_end_point=
         class_client = ClickUpClient()
         click_up_api_call_test = class_client.api_call_func(teams_end_point)
-        if click_up_api_call_test == None:
-                print(click_up_api_call_test)
+
 main()
 
         
